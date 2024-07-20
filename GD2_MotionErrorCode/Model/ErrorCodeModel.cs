42afd8e411eb2ca3798bbecfc9fe18f2a9992cf4
黃 御德 <asd281194533@gmail.com>
2024-07-20 19:31:49 +0800

    加入專案檔案。

---
+++ GD2_MotionErrorCode/Model/ErrorCodeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GD2_MotionErrorCode.Model
+{
+    public class ErrorCodeModel
+    {
+        public string HexCode {  get; set; }
+        public int Deccode {  get; set; }
+        public string Context {  get; set; }
+        public string Description { get; set; }
+
+
+    }
+}
